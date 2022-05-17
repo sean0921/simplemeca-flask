3ffebc31cfaa6b307d24f0e3b5451ae45770d13e
Sean Ho <sean.li.shin.ho@gmail.com>
2022-05-17 14:10:34 +0800

    chore: fix shellcheck warnings

--- test/local_gui_test.bash
+++ test/local_gui_test.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-ANSI_RED="\033[1;31m"
-ANSI_GREEN="\033[1;32m"
-ANSI_YELLOW="\033[1;33m"
-ANSI_END="\033[0m"
+read -r ANSI_RED < <(printf "\033[1;31m")
+read -r ANSI_GREEN < <(printf "\033[1;32m")
+read -r ANSI_YELLOW < <(printf "\033[1;33m")
+read -r ANSI_END < <(printf "\033[0m")
 trap ctrl_c INT
 
 ##################### functions
@@ -11,23 +11,23 @@ trap ctrl_c INT
 check_exit() {
     exit_status=$1
     if [ "${exit_status}" = "0" ]; then
-        printf "%s[INFO] Program normally terminated!%s" "${ANSI_GREEN}" "${ANSI_END}"
+        printf "%s[INFO] Program normally terminated!%s\n" "${ANSI_GREEN}" "${ANSI_END}"
     else
-        printf "%s[WARN] Program unnormally terminated! (exit code: %d)%s" "${ANSI_YELLOW}" "${exit_status}" "${ANSI_END}"
+        printf "%s[WARN] Program unnormally terminated! (exit code: %d)%s\n" "${ANSI_YELLOW}" "${exit_status}" "${ANSI_END}"
     fi
     exit "${exit_status}"
 }
 
 function ctrl_c()
 {
-    printf "${ANSI_YELLOW}[WARN] CTRL+C received, exit!${ANSI_END}\n"
+    printf "%s[WARN] CTRL+C received, exit!%s\n" "${ANSI_YELLOW}" "${ANSI_END}"
     check_exit 0
 }
 
 check_program() {
     my_program=$1
-    if [ -z $(command -v ${my_program}) ]; then
-        printf "${ANSI_RED}[ERR] Please Install ${my_program}!${ANSI_END}\n"
+    if [ -z "$(command -v "${my_program}")" ]; then
+        printf "%s[ERR] Please Install ${my_program}!%s\n" "${ANSI_RED}" "${ANSI_END}"
         check_exit 1
     fi
 }
@@ -35,7 +35,7 @@ check_program() {
 check_value() {
     my_value=$1
     if [[ $my_value == "" ]]; then
-        printf "${ANSI_RED}[ERR] Check your payload!${ANSI_END}\n"
+        printf "%s[ERR] Check your payload!%s\n" "${ANSI_RED}" "${ANSI_END}"
         check_exit 1
     fi
 }
@@ -51,46 +51,47 @@ check_program zenity
 check_program jq
 
 my_shell=$(ps -p $$|awk 'NR>1{print $4}')
-if [ $my_shell != bash ]; then
-    printf "${ANSI_RED}[ERR] Shell is not correct! ${ANSI_YELLOW}(Should be bash)${ANSI_END}\n"
+if [ "${my_shell}" != bash ]; then
+    printf "%s[ERR] Shell is not correct! %s(Should be bash)%s\n" "${ANSI_RED}" "${ANSI_YELLOW}" "${ANSI_END}"
     check_exit 1
 fi
 
 declare -a form_output
-declare -a color_code_rgb
 
-form_output=( $(zenity --forms --title="Type your Beachball Info" \
-        --text="Enter information about your beachball." \
+IFS=" " read -r -a form_output <<< "$(zenity --forms --title='Type your Beachball Info' \
+        --text='Enter information about your beachball.' \
         --separator=" " \
-        --add-entry="Strike" \
-        --add-entry="dip" \
-        --add-entry="rake" \
-        --add-entry="Title") )
+        --add-entry='Strike' \
+        --add-entry='dip' \
+        --add-entry='rake' \
+        --add-entry='Title')"
+
 case $? in
     0)
-        printf "${ANSI_GREEN}[INFO] Payload added.${ANSI_END}\n"
+        printf "%s[INFO] Payload added.%s\n" "${ANSI_GREEN}" "${ANSI_END}"
         ;;
     1)
-        printf "${ANSI_RED}[ERR] Program interupted.${ANSI_END}\n"
+        printf "%s[ERR] Program interupted.%s\n" "${ANSI_RED}" "${ANSI_END}"
         check_exit 1
 	;;
     -1)
-        printf "${ANSI_RED}[ERR] An unexpected error has occurred.${ANSI_END}\n"
+        printf "%s[ERR] An unexpected error has occurred.%s\n" "${ANSI_RED}" "${ANSI_END}"
         check_exit 1
 	;;
 esac
 
-color_code_rgb=$(zenity --color-selection --show-palette --title="Choose Your Beachball Color")
+color_code_rgb=$(zenity --color-selection --show-palette --title='Choose Your Beachball Color')
 case $? in
          0)
-		printf "${ANSI_GREEN}[INFO] You selected ${ANSI_YELLOW}${color_code_rgb}${ANSI_GREEN}.${ANSI_END}\n"
+		printf "%s[INFO] You selected %s%s%s.%s\n" "${ANSI_GREEN}" "${ANSI_YELLOW}" "${color_code_rgb}" "${ANSI_GREEN}" "${ANSI_END}"
                 ;;
          1)
-                printf "${ANSI_YELLOW}[WARN] No color selected.${ANSI_END}\n"
+                printf "%s[WARN] No color selected.%s\n" "${ANSI_YELLOW}" "${ANSI_END}"
                 color_code_rgb="rgb(0,0,0)"
                 ;;
         -1)
-                printf "${ANSI_RED}[ERR] An unexpected error has occurred.${ANSI_END}\n"
+                printf "%s[ERR] An unexpected error has occurred.%s\n" "${ANSI_RED}" "${ANSI_END}"
+                color_code_rgb="rgb(0,0,0)"
                 check_exit 1
                 ;;
 esac
@@ -100,17 +101,17 @@ dip=${form_output[1]}
 rake=${form_output[2]}
 title=${form_output[3]}
 
-color_code=( $(echo ${color_code_rgb} | sed -e 's/rgb(//' -e 's/)//' -e 's/,/ /g') )
+IFS=" " read -r -a color_code <<< "$(echo ${color_code_rgb} | sed -e 's/rgb(//' -e 's/)//' -e 's/,/ /g')"
 color_r=${color_code[0]}
 color_g=${color_code[1]}
 color_b=${color_code[2]}
 
-check_value $strike
-check_value $dip
-check_value $rake
-check_value $color_r
-check_value $color_g
-check_value $color_b
+check_value "${strike}"
+check_value "${dip}"
+check_value "${rake}"
+check_value "${color_r}"
+check_value "${color_g}"
+check_value "${color_b}"
 #check_value $title
 
 cat > payload.json << END
@@ -125,12 +126,12 @@ cat > payload.json << END
 }
 END
 
-( cat payload.json | jq -C . ) || ( printf "${ANSI_RED}Check your payload!${ANSI_END}\n" ; check_exit 1)
+( jq -C . payload.json ) || ( "%s[ERR] Check your payload!%s\n" "${ANSI_RED}" "${ANSI_END}" ; check_exit 1)
 
 if [ "$(curl -s -L http://127.0.0.1:5000/simplemeca)" == "Hello World! Welcome to SimpleMeca Service!" ]; then
-    printf "${ANSI_GREEN}[INFO] Service is up!${ANSI_END}\n"
+    printf "%s[INFO] Service is available!%s\n" "${ANSI_GREEN}" "${ANSI_END}"
 else
-    printf "${ANSI_RED}[ERR] Please Check Local Service is UP!${ANSI_END}\n"
+    printf "%s[ERR] Please Check Local Service is UP!%s\n" "${ANSI_RED}" "${ANSI_END}"
     check_exit 1
 fi
 
@@ -139,22 +140,22 @@ image_url_value=$(curl -s \
     -H 'Content-Type: application/json' \
     -d @'payload.json' \
     -L "http://127.0.0.1:5000/simplemeca" \
-    | jq '.image_url') || ( printf "${ANSI_RED}Check your payload!${ANSI_END}\n"; check_exit 1)
+    | jq '.image_url') || ( printf "%s[ERR] Check your payload!%s\n" "${ANSI_RED}" "${ANSI_END}"; check_exit 1)
 
-image_url=$(echo ${image_url_value} | tr -d  '"')
-printf "${ANSI_GREEN}[INFO] image URL is: ${ANSI_YELLOW}${image_url}${ANSI_END}\n"
+image_url=$(echo "${image_url_value}" | tr -d  '"')
+printf "%s[INFO] image URL is: %s%s$%s\n" "${ANSI_GREEN}" "${ANSI_YELLOW}" "${image_url}" "${ANSI_END}"
 
-if [ $(command -v eog) ]; then
-    eog -n ${image_url}
+if [ "$(command -v eog)" ]; then
+    eog -n "${image_url}"
     check_exit 0
 else
-    if [ $(command -v eom) ]; then
-        eom ${image_url}
+    if [ "$(command -v eom)" ]; then
+        eom "${image_url}"
         check_exit 0
     fi
 fi
 
-xdg-open ${image_url}
+xdg-open "${image_url}"
 
 # shutdown test server
 check_exit 0
